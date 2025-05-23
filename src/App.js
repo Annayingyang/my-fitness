5533b23f59d30356ef530e50d3d93834a0c000f5
Annayingyang <2540640@students.wits.ac.za>
2025-05-23 23:52:45 +0200

    routing is working now
    working on dynaic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Goals from './pages/Goals';
 import Workout from './pages/Workout';
@@ -8,23 +8,25 @@ import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div>
-      <nav>
-        <Link to="/">Dashboard</Link> | 
-        <Link to="/goals">Goals</Link> | 
-        <Link to="/workout">Workout</Link> | 
-        <Link to="/food-log">Food Log</Link> | 
-        <Link to="/profile">Profile</Link>
-      </nav>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Dashboard</Link> |{' '}
+          <Link to="/goals">Goals</Link> |{' '}
+          <Link to="/workout">Workout</Link> |{' '}
+          <Link to="/food-log">Food Log</Link> |{' '}
+          <Link to="/profile">Profile</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/goals" element={<Goals />} />
-        <Route path="/workout" element={<Workout />} />
-        <Route path="/food-log" element={<FoodLog />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/goals" element={<Goals />} />
+          <Route path="/workout" element={<Workout />} />
+          <Route path="/food-log" element={<FoodLog />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
